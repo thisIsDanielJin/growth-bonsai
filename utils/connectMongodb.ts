5b8c086795da48ce6414b999a7a72f4113877d13
thisIsDanielJin <danielwodke25@gmail.com>
2024-06-08 23:09:09 +0200

    feat: changing mongodb client

--- utils/connectMongodb.ts
+++ utils/connectMongodb.ts
@@ -5,15 +5,14 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-};
+
+const options = {};
 
 let clientPromise: Promise<MongoClient>;
+
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment");
+}
 
 clientPromise = new MongoClient(uri, options).connect();
 
