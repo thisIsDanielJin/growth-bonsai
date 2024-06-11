b8f416b79f5106020da3e6c22de6fde011318547
thisIsDanielJin <danielwodke25@gmail.com>
2024-06-11 17:16:04 +0200

    feat: remove debugging

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,19 +3,15 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    console.log("Try entered");
     const client = await clientPromise;
     const db = client.db("Application");
     const collection = db.collection("binaryGoals");
-    console.log("connection established");
     const updateResult = await collection.updateMany(
       {},
       { $set: { today_checked: false } }
     );
-    console.log("operation performed");
 
     client.close();
-    console.log("closed client");
     return NextResponse.json({
       message: "Successfully updated binaryGoals collection",
       modifiedCount: updateResult.modifiedCount,
